cf5a146e015ab8e6243025a1c4e4ce823b298769
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-26 15:00:39 +0200

    add wish-list

---
+++ src/org/lessons/java/wishlist/Main.java
@@ -0,0 +1,2 @@
+package org.lessons.java.wishlist;public class Main {
+}
---
+++ src/org/lessons/java/wishlist/Present.java
@@ -0,0 +1,6 @@
+package org.lessons.java.wishlist;
+
+public class Gift {
+    private String description;
+    private String 
+}
